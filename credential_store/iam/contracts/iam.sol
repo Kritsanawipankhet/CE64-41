0885cf02a838d530ff85ca01157fef19428732cf
KRITSANA-KMITL <kritsanawipankhet@icloud.com>
2021-09-18 05:31:05 +0700

    update IAM to be able to work in two way on testnet and localnet - testnet server using infura + ropsten - update compiler solidity v.0.8.0 for new feature support , (mapping return)

--- credential_store/iam/contracts/iam.sol
+++ credential_store/iam/contracts/iam.sol
@@ -1,22 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract iam {
-        
-    struct Users{
+    struct Users {
         string username;
         string password;
     }
-    
-    mapping (address => Users) users;
-    
-    function registerUsers(string memory _username,string memory _password) public {
-        
+    struct Services {
+        string services_name;
+    }
+    mapping(address => Users) public users;
+    mapping(uint256 => Services) services;
+
+    // string private name;
+
+    // constructor() public {
+    //     name = "KritsanaWipankhet";
+    //     users[0] = Users("kritsana", "password");
+    //     users[1] = Users("wipankhet", "password");
+    // }
+
+    function registerUsers(string memory _username, string memory _password)
+        public
+    {
         users[msg.sender].username = _username;
         users[msg.sender].password = _password;
+        // users[msg.sender].services[0].services_name = "Facebook";
     }
 
-    function getUsers() public view returns (string memory, string memory) {
-        return (users[msg.sender].username, users[msg.sender].password);
+    function getUsers(address _id) public view returns (Users memory) {
+        return users[_id];
     }
 }
