8e5cc2a1996ed746b65443111ad9935a6b3fe61a
KRITSANA-KMITL <kritsanawipankhet@icloud.com>
2021-09-18 05:49:50 +0700

    add nodejs file for test IAM - index.js used for localnet - index_testnet.js used for public testnet *** The results can be performed as desired.

--- credential_store/web3-test/index.js
+++ credential_store/web3-test/index.js
@@ -1,72 +1,33 @@
 const Web3 = require("web3");
-const web3 = new Web3(Web3.givenProvider || "ws://localhost:9545");
-
 const Contract = require("web3-eth-contract");
-Contract.setProvider("ws://localhost:9545");
+var iamJson = require("./iam.json");
 
-const contractAddress = "0xA03b67dD7d22C4Dc0970f30c8Cd9ae3372c36dF6";
-const abi = [
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "string",
-        name: "_username",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "_password",
-        type: "string",
-      },
-    ],
-    name: "registerUsers",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "getUsers",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-];
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+const contractAddress = "0x5A79D9609Ada194585C3Cb7bBEFe31a9bE70395a";
+Contract.setProvider("http://localhost:9545");
+var contract = new Contract(iamJson.abi, contractAddress);
 
-var contract = new Contract(abi, contractAddress);
-
-contract.methods
-  .getUsers()
-  .call(
-    { from: "0xA2b1e3779dC57Dbc22d9C768559d02dDbBE903a1" },
-    function (error, result) {
-      console.log(result);
-    }
-  );
+var account = "0xA2b1e3779dC57Dbc22d9C768559d02dDbBE903a1";
 
 contract.methods
   .registerUsers("Wipankhet", "0826165190")
-  .send(
-    { from: "0xA2b1e3779dC57Dbc22d9C768559d02dDbBE903a1" },
-    function (error, result) {
-      console.log(result);
-    }
-  )
-  .then(function (receipt) {
+  .send({
+    from: account,
+  })
+  .on("transactionHash", function (hash) {
+    console.log(hash);
+  })
+  .on("confirmation", function (confirmationNumber, receipt) {})
+  .on("receipt", function (receipt) {
     console.log(receipt);
+  })
+  .on("error", function (error, receipt) {
+    console.log(error, receipt);
+    // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+  });
+
+contract.methods
+  .getUsers(account)
+  .call({ from: account }, function (error, result) {
+    console.log(result);
   });
