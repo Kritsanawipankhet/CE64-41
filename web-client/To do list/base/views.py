8eedea20941fdce103f5e636d253ec067fd8ee6f
Phongpak Pudsorn <60436939+phongpak001@users.noreply.github.com>
2021-10-14 17:53:48 +0700

    add Share function

--- web-client/To do list/base/views.py
+++ web-client/To do list/base/views.py
@@ -25,6 +25,41 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True   
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Share, Task
+#from .forms import PositionForm
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
     def form_valid(self, form):
         user = form.save()
         if user is not None:
@@ -36,43 +71,73 @@ class RegisterPage(FormView):
             return redirect('tasks')
         return super(RegisterPage, self).get(*args, **kwargs)
 
-class TaskList(LoginRequiredMixin, ListView):
-    model = Task
-    context_object_name = 'tasks'
 
+class TaskList(LoginRequiredMixin, ListView):
+    model = Share
+    context_object_name = 'tasks','taskss'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
+        #context['tasks'] = context['tasks'].filter(share_target=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['count'] = context['tasks'].filter(title__icontains=search_input)
-        
+            context['tasks'] = context['tasks'].filter(
+                title__contains=search_input)
+
+        context['search_input'] = search_input
+
+        return context
+
+class TaskLists(LoginRequiredMixin, ListView):
+    model = Share
+    context_object_name = 'taskss'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['taskss'] = context['taskss'].filter(share_target=self.request.user)
+        context['count'] = context['taskss'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['taskss'] = context['taskss'].filter(
+                title__contains=search_input)
+
         context['search_input'] = search_input
 
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
-    model = Task
+    model = Share
     context_object_name = 'task'
+    template_name = 'base/task.html'
+
 
 class TaskCreate(LoginRequiredMixin, CreateView):
-    model = Task
-    fields = ['title','description','complete']
+    model = Share
+    fields = ['title', 'description', 'complete','share_target']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
+
 class TaskUpdate(LoginRequiredMixin, UpdateView):
-    model = Task
-    fields = '__all__'
+    model = Share
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class TaskDelete(LoginRequiredMixin, DeleteView):
-    model = Task
+class ShareUpdate(LoginRequiredMixin, UpdateView):
+    model = Share
+    fields = ['share_target']
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(LoginRequiredMixin, DeleteView):
+    model = Share
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
-
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(user=owner)
