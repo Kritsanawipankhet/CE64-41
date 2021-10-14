6366feba9ab16bcd60a0e4bf86c31b68fd12afe0
Phongpak Pudsorn <60436939+phongpak001@users.noreply.github.com>
2021-10-14 17:58:26 +0700

    add Share TaskList and Update

--- web-client/To do list/base/views.py
+++ web-client/To do list/base/views.py
@@ -1,31 +1,4 @@
-from django.shortcuts import redirect, render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-
-from .models import Task
-# Create your views here.
-class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
-    fields = '__all__'
-    redirect_authenticated_user = True
-
-    def get_success_url(self):
-        return reverse_lazy('tasks')
-
-class RegisterPage(FormView):
-    template_name = 'base/register.html'
-    form_class = UserCreationForm
-    redirect_authenticated_user = True   
-    success_url = reverse_lazy('tasks')
-
-    def form_valid(self, form):from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
