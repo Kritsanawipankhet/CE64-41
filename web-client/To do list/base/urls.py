f6fb42358456e94e38a95471e9b576c24720fa18
Phongpak Pudsorn <60436939+phongpak001@users.noreply.github.com>
2021-10-14 18:00:40 +0700

    add another page URLs for Share method

--- web-client/To do list/base/urls.py
+++ web-client/To do list/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
+from .views import CustomLoginView, TaskList, TaskLists, TaskDetail, TaskCreate, TaskUpdate, ShareUpdate, DeleteView, CustomLoginView, RegisterPage
 
 from django.contrib.auth.views import LogoutView
 
@@ -8,8 +8,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page = 'login') ,name = 'logout'),
     path('register/', RegisterPage.as_view() ,name = 'register'),
     path('', TaskList.as_view() ,name = 'tasks'),
+    path('share/', TaskLists.as_view() ,name = 'taskss'),
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name = 'task-delete'),
+    path('share-update/<int:pk>/', ShareUpdate.as_view(), name = 'share-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name = 'task-delete'),
 ]
