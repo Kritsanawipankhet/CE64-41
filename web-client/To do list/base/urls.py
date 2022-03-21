3c57eb0a96cf0dc8da7af9066d3467bbb98a3ec4
Phongpak Pudsorn <60436939+phongpak001@users.noreply.github.com>
2022-03-21 11:39:02 +0700

    Update urls.py (#45)

--- web-client/To do list/base/urls.py
+++ web-client/To do list/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CustomLoginView, TaskList, TaskLists, ShareList, TaskDetail, ShareDetail, TaskCreate, ShareCreate, TaskUpdate, ShareUpdate, SharingUpdate, TaskDeleteView, ShareDeleteView, CustomLoginView, RegisterPage
+from .views import CustomLoginView, TaskList, TaskLists, ShareList, TaskDetail, ShareDetail, TaskCreate, ShareCreate, TaskUpdate, SuperTaskUpdate, ShareUpdate, SharingUpdate, TaskDeleteView, ShareDeleteView, CustomLoginView, RegisterPage
 
 from django.contrib.auth.views import LogoutView
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('share-create/', ShareCreate.as_view(), name = 'share-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
+    path('super-task-update/<int:pk>/', SuperTaskUpdate.as_view(), name = 'super-task-update'),
     path('share-update/<int:pk>/', ShareUpdate.as_view(), name = 'share-update'),
     path('sharing-update/<int:pk>/', SharingUpdate.as_view(), name = 'sharing-update'),
     path('task-delete/<int:pk>/', TaskDeleteView.as_view(), name = 'task-delete'),
