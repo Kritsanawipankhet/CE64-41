37f317636117cce0a1816b0ac371bc59e66114a9
Phongpak Pudsorn <60436939+phongpak001@users.noreply.github.com>
2022-03-21 09:34:37 +0700

    Update models.py

--- web-client/To do list/base/models.py
+++ web-client/To do list/base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import rules
+from rules.contrib.models import RulesModel
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -7,20 +9,25 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['complete']
         
-class Share(Task):
-    share_target = models.ManyToManyField(User, blank=True)
-    
 class Sharing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    task_name = models.ForeignKey(Task, on_delete=models.CASCADE, null=True, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True, blank=True)
     target = models.CharField(max_length=150)
-    
+    view_only = models.BooleanField(default=False)
     def __str__(self):
-        return self.task_name
+        return self.target
+    
+class TestModel(RulesModel):
+    class Meta:
+        rules_permissions = {"add": rules.always_true, "view": rules.always_true}
+        
+    @classmethod
+    def preprocess_rules_permissions(cls, perms):
+        perms["custom"] = rules.always_true
